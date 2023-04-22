a15100be64925b709f4b633ad5c7f3867e0d8364
Robert Piechowski <robert.t.piechowski@gmail.com>
2023-04-22 22:37:44 +0200

    setup

---
+++ src/main/java/com/example/nbpinternshiptask/NbpInternshipTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbpinternshiptask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpInternshipTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbpInternshipTaskApplication.class, args);
+    }
+
+}
